d6741ba09205095f8a289c5e562dd179a47ea192
writerity-dragana <writerity.dragana@gmail.com>
2020-03-13 11:41:55 +0100

    remove

--- backend/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('MAIL_ADDRESS', 'writerity.dragana@gmail.com');
-define('PASSWORD', 'patlidzan1');
-define('FROM_ADDRESS', 'writerity.dragana@gmail.com');
-define('TO_ADDRESS', 'dragana.spasovska94@gmail.com');
